09b8c371165bbca4e2b7d7f5128e982bae1fbd96
thanhdat444 <datkim2222@gmail.com>
2026-08-20 19:17:03 +0700

    day1-task2

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:3000","http://localhost:5173"]
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
